59b19438070146b1b4a78a0e7a79b1c878bc273a
Alexandr Pakhomov <palex068@gmail.com>
2023-04-20 13:40:04 +0300

    ДЗ Семинар2

--- L001.py
+++ L001.py
@@ -1,5 +1,6 @@
 import numpy as np
 from math import factorial as fc
+import math
 from fractions import Fraction
 
 def c (k,n):
@@ -11,7 +12,8 @@ def a (k,n):
 def p (n):
     return fc(n)
 
-answer = Fraction(c(1,5)*c(2,5)*c(3,10)/c(6,20))
+# answer = Fraction(c(1,5)*c(2,5)*c(3,10)/c(6,20))
+answer = (3**3 / fc(3)) * math.e**(-3)
 
 # print(c(4,7)*c(1,3)/c(5,10))
 # print(fc(8)/fc(3)*fc(2))
